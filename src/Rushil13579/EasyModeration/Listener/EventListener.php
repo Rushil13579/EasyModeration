30edc580d1375b741aba6c011ee27f8894a0a2d1
Rushil13579 <61579586+Rushil13579@users.noreply.github.com>
2021-04-21 13:14:38 +0530

    Add files via upload

--- src/Rushil13579/EasyModeration/Listener/EventListener.php
+++ src/Rushil13579/EasyModeration/Listener/EventListener.php
@@ -10,18 +10,7 @@ use pocketmine\{
 use pocketmine\command\ConsoleCommandSender;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\{
-    PlayerJoinEvent,
-    PlayerQuitEvent,
-    PlayerChatEvent,
-    PlayerMoveEvent,
-    PlayerCommandPreprocessEvent
-};
-use pocketmine\event\entity\{
-    EntityDamageEvent,
-    EntityDamageByEntityEvent
-};
-use pocketmine\event\server\CommandEvent;
+use pocketmine\event\player\PlayerChatEvent;
 
 use DateTime;
 
@@ -35,81 +24,6 @@ class EventListener implements Listener {
 
     public function __construct(Main $main){
         $this->main = $main;
-    }
-
-    public function altJoin(PlayerJoinEvent $ev){
-        $player = $ev->getPlayer();
-
-        if(!file_exists($this->main->getDataFolder() . 'alts/' . $player->getAddress())){
-            file_put_contents($this->main->getDataFolder() . 'alts/' . $player->getAddress(), $player->getName() . ",\n");
-            $this->main->getLogger()->info('§aNo records found for ' . $player->getName() . '!');
-            return null;
-        }
-
-        $file = explode(",\n", file_get_contents($this->main->getDataFolder() . "alts/" . $player->getAddress(), true));
-
-		if(!in_array($player->getName(), $file)){
-			file_put_contents($this->main->getDataFolder() . "alts/" . $player->getAddress(), $player->getName() . ",\n", FILE_APPEND);
-			$this->main->getLogger()->info('§4' . $player->getName() . ' might be an alt account!');
-            return null;
-		} else {
-            if(count($file) > 2){
-                $this->main->getLogger()->info('§4' . $player->getName() . ' might be an alt account!');
-                return null;
-            } else {
-                $this->main->getLogger()->info('§aNo records found for ' . $player->getName() . '!');
-                return null;
-            }
-        }
-    }
-
-    public function quitFrozen(PlayerQuitEvent $ev){
-        $player = $ev->getPlayer();
-
-        if(isset($this->main->frozen[$player->getName()])){
-            if($this->main->cfg->get('frozen-quit-commands') !== null and is_array($this->main->cfg->get('frozen-quit-commands'))){
-                foreach($this->main->cfg->get('frozen-quit-commands') as $cmd){
-                    $cmd = str_replace('{player}', $player->getName(), $cmd);
-                    $this->main->getServer()->dispatchCommand(new ConsoleCommandSender(), $cmd);
-                }
-            }
-        }
-        unset($this->main->frozen[$player->getName()]);
-    }
-
-    public function staffChat(PlayerChatEvent $ev){
-        if($ev->isCancelled()){
-            return null;
-        }
-
-        $player = $ev->getPlayer();
-        $msg = $ev->getMessage();
-
-        if($player->hasPermission('easymoderation.staffchat')){
-            $array = explode(' ', trim($msg));
-            $hash = $array[0];
-            if($hash == '#'){
-                $nmsg = str_replace('# ', '', $msg);
-                foreach($this->main->getServer()->getOnlinePlayers() as $pl){
-                    if($pl->hasPermission('easymoderation.staffchat')){
-                        $pl->sendMessage('§4[§cSC§4] §b> §6' . $player->getName() . ': §d' . $nmsg);
-                    }
-                }
-                $this->main->getLogger()->info('§4[§cSC§4] §b> §6' . $player->getName() . ': §d' . $nmsg);
-                $ev->setCancelled();
-
-            }
-        }
-
-        if(isset($this->main->staffchat[$player->getName()])){
-            foreach($this->main->getServer()->getOnlinePlayers() as $pl){
-                if($pl->hasPermission('easymoderation.staffchat')){
-                    $pl->sendMessage('§4[§cSC§4] §b> §6' . $player->getName() . ': §d' . $msg);
-                }
-            }
-            $this->main->getLogger()->info('§4[§cSC§4] §b> §6' . $player->getName() . ': §d' . $msg);
-            $ev->setCancelled();
-        }
     }
 
     public function isMutedChat(PlayerChatEvent $ev){
@@ -143,60 +57,6 @@ class EventListener implements Listener {
             $ev->setCancelled();
             $player->sendMessage($muteMsg);
         }
-
-        if($ipmuteList->isBanned($player->getAddress())){
-
-            $entries = $ipmuteList->getEntries();
-            $entry = $entries[strtolower($player->getAddress())];
-            $reason = $entry->getReason();
-            $muteMsg = '';
-
-            if($entry->getExpires() === null){
-                $muteMsg = "§4You are ip muted forever for §c$reason";
-            } else {
-                if($entry->hasExpired()){
-                    $ipmuteList->remove($player->getAddress());
-                    $player->sendMessage('§aYour ip mute has expired!');
-                    return null;
-                }
-
-                $expiryToString = Expiry::expirationTimerToString($entry->getExpires(), new DateTime());
-                $muteMsg = "§4You are ip muted until §c$expiryToString §4for §c$reason";
-            }
-            $ev->setCancelled();
-            $player->sendMessage($muteMsg);
-        }
-    }
-
-    public function muteChat(PlayerChatEvent $ev){
-        if($ev->isCancelled()){
-            return null;
-        }
-
-        $player = $ev->getPlayer();
-
-        if(isset($this->main->mutechat) and $this->main->mutechat == 'on'){
-            if(!$player->hasPermission('easymoderation.mutechat.bypass')){
-                $player->sendMessage('§cGlobal chat is muted! You cannot talk!');
-                $ev->setCancelled();
-                return false;
-            }
-        }
-    }
-
-    public function frozenChat(PlayerChatEvent $ev){
-        if($ev->isCancelled()){
-            return null;
-        }
-
-        $player = $ev->getPlayer();
-
-        if(isset($this->main->frozen[$player->getName()])){
-            if($this->main->cfg->get('disable-chat-while-frozen') == 'true'){
-                $player->sendMessage('§cYou cannot do this while being frozen');
-                $ev->setCancelled();
-            }
-        }
     }
 
     public function chatSensor(PlayerChatEvent $ev){
@@ -212,11 +72,6 @@ class EventListener implements Listener {
                 if(strpos(strtolower($msg), strtolower($censorword)) !== false){
                     $player->sendMessage('§cYou cannot use that word!');
                     $ev->setCancelled();
-                    if($this->main->cfg->get('chat-censor-discord-post') == 'enabled'){
-                        $webhook = $this->main->cfg->get('chat-censor-webhook');
-                        $msg = "__**NEW MESSAGE CENSORED**__\nMessage: $msg\nMessage Sender: " . $player->getName();
-                        $this->main->postToDiscord($webhook, $msg);
-                    }
                 }
             }
         }
@@ -244,102 +99,6 @@ class EventListener implements Listener {
                     $this->main->chatinterval[$player->getName()] = time() + $interval;
                 }
             }
-        }
-    }
-
-    public function frozenMove(PlayerMoveEvent $ev){
-        $player = $ev->getPlayer();
-
-        if(isset($this->main->frozen[$player->getName()])){
-            $player->setImmobile();
-        }
-    }
-
-    public function frozenCommand(PlayerCommandPreprocessEvent $ev){
-        if($ev->isCancelled()){
-            return null;
-        }
-
-        $player = $ev->getPlayer();
-        $cmd = $ev->getMessage();
-
-        $ncmd = str_replace('/', '', strtok($cmd, ' '));
-
-        if(isset($this->main->frozen[$player->getName()])){
-            if($this->main->cfg->get('frozen-disabled-commands') !== null and is_array($this->main->cfg->get('frozen-disabled-commands'))){
-
-                if($this->main->cfg->get('frozen-disabled-commands')[0] == '*'){
-                    $player->sendMessage('§cYou cannot do this while being frozen');
-                    $ev->setCancelled();
-                    return null;
-                }
-            
-                if(!in_array($ncmd, $this->main->cfg->get('frozen-disabled-commands'))){
-                    $player->sendMessage('§cYou cannot do this while being frozen');
-                    $ev->setCancelled();
-                }
-            }
-        }
-    }
-
-    public function vanishDamage(EntityDamageEvent $ev){
-        $player = $ev->getEntity();
-
-        if(!$player instanceof Player){
-            return null;
-        }
-
-        if(isset($this->main->vanish[$player->getName()])){
-            $ev->setCancelled();
-        }
-    }
-
-    public function frozenDamage(EntityDamageEvent $ev){
-        $player = $ev->getEntity();
-
-        if(!$player instanceof Player){
-            return null;
-        }
-
-        if(isset($this->main->frozen[$player->getName()])){
-            if($this->main->cfg->get('frozen-damage-disabled') == 'true'){
-                $ev->setCancelled();
-            }
-        }
-
-        if($ev instanceof EntityDamageByEntityEvent){
-            $damager = $ev->getDamager();
-
-            if(!$damager instanceof Player){
-                return null;
-            }
-
-            if(isset($this->main->frozen[$damager->getName()])){
-                if($this->main->cfg->get('frozen-damage-disabled') == 'true'){
-                    $damager->sendMessage('§cYou cannot do this while being frozen');
-                    $ev->setCancelled();
-                }
-            }
-        }
-    }
-
-    public function spyCommand(CommandEvent $ev){
-        if($ev->isCancelled()){
-            return null;
-        }
-
-        $sender = $ev->getSender();
-        $cmd = $ev->getCommand();
-
-        if($sender instanceof Player){
-            if(!$sender->hasPermission('easymoderation.spy.exempt')){
-                foreach($this->main->getServer()->getOnlinePlayers() as $player){
-                    if(isset($this->main->spy[$player->getName()])){
-                        $player->sendMessage('§4Spy 》 §6' . $sender->getName() . ': §d/' . $cmd);
-                    }
-                }
-            }
-            $this->main->getLogger()->info('§4Spy 》 §6' . $sender->getName() . ': §d/' . $cmd);
         }
     }
 }
--- src/Rushil13579/EasyModeration/Main.php
+++ src/Rushil13579/EasyModeration/Main.php
@@ -12,11 +12,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
 use Rushil13579\EasyModeration\Commands\{
-    IpBan, TempIpBan, IpUnban, Permban, Tempban,
-    Unban, Kick, Kickall, IpMute, IpUnmute,
-    Mute, Unmute, Mutelist, Mutechat, Warn,
-    Report, Alts, Spy, Staffchat, Vanish,
-    Freeze, Unfreeze
+    IpBan, IpUnban, Permban, Tempban, Unban,
+    Kick, Mute, Unmute, Mutelist, Warn
 };
 
 use Rushil13579\EasyModeration\Discord\DiscordManager;
@@ -34,11 +31,6 @@ class Main extends PluginBase {
 
     public $mutechat;
     public $chatinterval = [];
-    public $reportcd = [];
-    public $spy = [];
-    public $staffchat = [];
-    public $frozen = [];
-    public $vanish = [];
 
     const PREFIX = '§3[§bEasyModeration§3]';
 
@@ -52,14 +44,12 @@ class Main extends PluginBase {
 
         $this->unregisterCommands();
         $this->registerCommands();
-
-		@mkdir($this->getDataFolder() . "alts/");
     }
 
     public function versionCheck(){
         if($this->cfg->get('plugin-version') !== '1.0.0'){
-            $this->getLogger()->warning('§cThe configuration file for EasyModeration is outdated! Please delete it and restart the server to install the latest version!');
-            $this->getServer()->getPluginManager()->disablePlugin($this);
+            rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'old_config.yml');
+            $this->getLogger()->warning('§cThe configuration file for EasyModeration was outdated so it has been renamed to old_config.yml and a new file has been generated!');
         }
     }
 
@@ -75,27 +65,15 @@ class Main extends PluginBase {
     public function registerCommands(){
         $cmdMap = $this->getServer()->getCommandMap();
         $cmdMap->register('EasyModeration', new IpBan($this));
-        $cmdMap->register('EasyModeration', new TempIpBan($this));
         $cmdMap->register('EasyModeration', new IpUnban($this));
         $cmdMap->register('EasyModeration', new Permban($this));
         $cmdMap->register('EasyModeration', new Tempban($this));
         $cmdMap->register('EasyModeration', new Unban($this));
         $cmdMap->register('EasyModeration', new Kick($this));
-        $cmdMap->register('EasyModeration', new Kickall($this));
-        $cmdMap->register('EasyModeration', new IpMute($this));
-        $cmdMap->register('EasyModeration', new IpUnmute($this));
         $cmdMap->register('EasyModeration', new Mute($this));
         $cmdMap->register('EasyModeration', new Unmute($this));
         $cmdMap->register('EasyModeration', new Mutelist($this));
-        $cmdMap->register('EasyModeration', new Mutechat($this));
         $cmdMap->register('EasyModeration', new Warn($this));
-        $cmdMap->register('EasyModeration', new Report($this));
-        $cmdMap->register('EasyModeration', new Alts($this));
-        $cmdMap->register('EasyModeration', new Spy($this));
-        $cmdMap->register('EasyModeration', new Staffchat($this));
-        $cmdMap->register('EasyModeration', new Vanish($this));
-        $cmdMap->register('EasyModeration', new Freeze($this));
-        $cmdMap->register('EasyModeration', new Unfreeze($this));
     }
 
     public static function getNameMutes() : MutedList {
